a96d8433c2f4d514f2c2cb1253e6eaf84b13f05b
lahuke <luke@luketucker.com>
2017-08-18 14:39:13 -0600

    success

--- js/app.js
+++ js/app.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
-$('ul').on('click', 'li', function(event){
-	$(this).toggleClass('linethrough');
+	$('#user-input').submit(function(event) {
+		event.preventDefault();
+		var something = $('#input-box').val();
+		// if user inputs nothing and hits enter, alert will pop up and nothing is appended
+		if (something === "") {
+			alert("It's hard to shop without a list! Please enter an item.")
+		}
+		//otherwise, text is appended as a li to the shopping list
+		else {
+			$('.all-items ul').append("<li class='current-item'> "+ something + "</li>");	
+		}
+		// resets the textbox after item is entered
+		$('#user-input')[0].reset();
+	});
+
 });
+
+//functions to delete and strikethrough code when checkbox and trashcan are clicked
+assignlisteners();
+function assignlisteners() {
+	$('ul').on('click', '.current-item', function(event) {
+		$(this).toggleClass('linethrough');
+		console.log('item checked');
+	});
+}
